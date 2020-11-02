f96c1a4ab2774a9859692152e533e6dd268f2ef3
Jess Chin <jmalvinchin@gmail.com>
2020-11-02 14:06:29 +0800

    Updates

---
+++ precache-manifest.a34c6f1cf4e73273a63797d49766199a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd25177f1fe2e5a1c52bd12970aa647",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "ed259ce9c9012054fee2",
+    "url": "/react-to-do/static/css/main.2f8876da.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/react-to-do/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed259ce9c9012054fee2",
+    "url": "/react-to-do/static/js/main.4aed40d5.chunk.js"
+  },
+  {
+    "revision": "dce55eee1c1ea4d16e6e",
+    "url": "/react-to-do/static/js/runtime-main.a5801b6d.js"
+  }
+]);
